09f28f65b44f5409be6a070aaec78a5d4185d6cc
Wendson <wendsonsantanajps@gmail.com>
2021-10-24 16:33:17 -0300

    LoginBox

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -4,10 +4,12 @@ import styles from './styles.module.scss'
 
 
 export function LoginBox(){
+    const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=51f9b6752cbea82a8d73`
+
     return (
         <div className={styles.loginBoxWrapper}>
             <strong>Login and share your message</strong>
-            <a href="#" className={styles.signInWithGithub}>
+            <a href={signInUrl} className={styles.signInWithGithub}>
                 <VscGithubInverted size="24" />
                 Login with Github
             </a>
