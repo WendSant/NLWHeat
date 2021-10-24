d079befb882143c7ff4f9e5535a3c8f8382d1bf0
Wendson <wendsonsantanajps@gmail.com>
2021-10-24 19:15:03 -0300

    Test

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,10 +1,50 @@
 
+import { sign } from 'jsonwebtoken';
+import { useEffect } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc';
+import { api } from '../../services/api';
 import styles from './styles.module.scss'
+
+type AuthResponse = {
+    token: string;
+    user: {
+        id: string;
+        avatar_url: string;
+        name: string;
+        login: string;
+    }
+}
 
 
 export function LoginBox(){
     const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=51f9b6752cbea82a8d73`
+
+    async function signIn(githubCode: string) {
+       const response = await api.post<AuthResponse>('authenticate', {
+            code: githubCode,
+        })
+
+        const { token, user} = response.data;
+
+        localStorage.setItem('@dowhile:token', token);
+        console.log(user);
+
+    }
+
+
+    useEffect(() => {
+        const url = window.location.href;
+        const hasGithubCode = url.includes('?code=');
+
+        if (hasGithubCode){
+            const [ urlWithoutCode, githubCode] = url.split('?code=');
+
+            window.history.pushState({}, '', urlWithoutCode);
+
+            signIn(githubCode);
+
+        }
+    }, [])
 
     return (
         <div className={styles.loginBoxWrapper}>
