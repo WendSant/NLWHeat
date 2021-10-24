285167b70223ff83af18b3e4260995be0508d9b1
Wendson <wendsonsantanajps@gmail.com>
2021-10-24 16:24:38 -0300

    Integration front with back messagelist

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,40 +1,51 @@
-import styles from './styles.module.scss';
+import { api } from "../../services/api";
 
-import logoImg from '../../assets/logo.svg';
+import styles from "./styles.module.scss";
 
-export function MessageList(){
-    return (
-        <div className={styles.messageListWrapper}>
-            <img src={logoImg} alt="DoWhile2021" />
-            <ul className={styles.messageList}>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://www.github.com/wendsant.png" alt="Wendson Santana" />
-                        </div>
-                        <span>Wendson Santana</span>
-                    </div>
-                </li>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://www.github.com/wendsant.png" alt="Wendson Santana" />
-                        </div>
-                        <span>Wendson Santana</span>
-                    </div>
-                </li>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://www.github.com/wendsant.png" alt="Wendson Santana" />
-                        </div>
-                        <span>Wendson Santana</span>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    )
-} 
+import logoImg from "../../assets/logo.svg";
+import { useEffect, useState } from "react";
+
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+};
+
+export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    api.get<Message[]>("messages/last3").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile2021" />
+      <ul className={styles.messageList}>
+        {messages.map((message) => {
+          return (
+            <li key={message.id} className={styles.message}>
+              <p className={styles.messageContent}>
+                {message.text}
+              </p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img
+                    src={message.user.avatar_url}
+                    alt={message.user.name}
+                  />
+                </div>
+                <span>Wendson Santana</span>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
